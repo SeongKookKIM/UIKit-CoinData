beb19c5607b8771bd620d7426699f318ac2c620a
SeongKookKIM <ksk8646@naver.com>
2024-06-26 23:08:17 +0900

    ReadMe 수정

--- CoinApp/App/SceneDelegate.swift
+++ CoinApp/App/SceneDelegate.swift
@@ -13,30 +13,23 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     
     
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
+
         guard let windowScene = (scene as? UIWindowScene) else { return }
         let window = UIWindow(windowScene: windowScene)
         
+        /*
          // 기존 네이게이션 컨트롤러
          let rootViewcontroller = LoginViewController()
          let navigationCV = UINavigationController(rootViewController: rootViewcontroller)
          
          window.rootViewController = navigationCV
          window.makeKeyAndVisible()
-         
-        
+         */
 
-        /*
          // PlashView
          let splashVC = SplashViewController()
          window.rootViewController = splashVC
          window.makeKeyAndVisible()
-         */
-        
-
-        
         
         self.window = window
     }
